34a8bbcfb4b78ede846001ea3e2fc5183ee25d56
SvajunasKa <manosuoLuna1>
2019-11-05 17:33:23 +0200

    sign in

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import './preview-collection.style.scss'
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+import './preview-collection.style.scss';
 
 const PreviewCollection = ({title, items}) => (
 	<div className='collection-preview'>
@@ -8,8 +9,8 @@ const PreviewCollection = ({title, items}) => (
 			{
 				items
 					.filter((item, idx) => idx < 4)
-					.map((item) =>(
-				<div key={item.id}>{item.name}</div>
+					.map(({id, ...otherItemProps}) =>(
+				<CollectionItem key={id} {...otherItemProps}/>
 				))
 			}
 		</div>
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../../components/custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			email: '',
+			password: ''
+		}
+	}
+
+	handleSubmit = event =>{
+		event.preventDefault();
+	};
+
+	handleChange = event =>{
+		const {name, value} = event.target;
+
+		this.setState({[name]: value});
+	};
+
+	render(){
+		return(
+			<div className='sign-in'>
+				<h2>I already have an account</h2>
+				<span>Sign with out email an password</span>
+				<form onSubmit={this.handleSubmit}>
+					<FormInput
+						name="email"
+						type="email"
+						value={this.state.email}
+						handleChange={this.handleChange}
+						label='email'
+						required/>
+					<FormInput
+						name="password"
+						type="password"
+						value={this.state.password}
+						handleChange={this.handleChange}
+						label='password'
+						required/>
+
+					<CustomButton type='submit' value='Submit form'>
+						Sign In
+					</CustomButton>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default SignIn;
