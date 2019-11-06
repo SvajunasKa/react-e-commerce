82c11148cccc258ac6f159505f032bff1d22112b
Svajunas Kaseta <svajunas@aventusgroup.com>
2019-11-06 20:49:08 +0200

    sigin

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../../components/custom-button/custom-button.component';
 
-import {signWithGoogle} from "../../firebase/firebase.utils";
+import {auth, signWithGoogle} from "../../firebase/firebase.utils";
 
 import './sign-in.styles.scss';
 
@@ -16,8 +16,17 @@ class SignIn extends React.Component {
 		}
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
+
+		const {email, password} = this.state;
+
+		try{
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({email: '', password: ''});
+		}catch (e) {
+			console.log(e)
+		}
 	};
 
 	handleChange = event => {
